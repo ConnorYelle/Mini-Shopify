2a449fdd7b73921f818311858e89af66c52ebb9c
tommy <tommyphang@cmail.carleton.ca>
2025-11-01 22:57:52 -0400

    Revert "Completed Store.java, StoreController, StoreControllerTest, and storeTest for milestone 1"
    This reverts commit bd5b99a6c85f55bcba9b7f3b0b502e9832cf9978.

--- src/main/java/com/sysc4806/mini_shopify/Product.java
+++ src/main/java/com/sysc4806/mini_shopify/Product.java
@@ -1,20 +1,4 @@
 package com.sysc4806.mini_shopify;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-
-@Entity
 public class Product {
-    @Id
-    private Long id;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Product(){}
 }
--- src/main/java/com/sysc4806/mini_shopify/StoreController.java
+++ src/main/java/com/sysc4806/mini_shopify/StoreController.java
@@ -1,29 +1,13 @@
 package com.sysc4806.mini_shopify;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Optional;
-
-@RestController
+@Controller
 public class StoreController {
-    
-    @Autowired
-    private storeRepository storeRepository;
-    
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("title", "Shopify");
         return "index";
-    }
-    
-    @PostMapping
-    public Store createStore(@RequestBody Store store) {
-        return storeRepository.save(store);
-    }
-    
-    @GetMapping("/store/{id}")
-    public Optional<Store> getStore(@PathVariable Long id) {
-        return storeRepository.findById(id);
     }
 }
