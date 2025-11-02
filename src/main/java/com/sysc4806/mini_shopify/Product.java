f33c9f6bb96097b286fa3f60b7f6ddc686d53461
OwenMcKibbon1 <owenmckibbon@cmail.carleton.ca>
2025-11-02 18:53:49 -0500

    Created product repository, filled ProductController, and added fields to product

--- src/main/java/com/sysc4806/mini_shopify/Product.java
+++ src/main/java/com/sysc4806/mini_shopify/Product.java
@@ -13,6 +13,12 @@ public class Product {
     @JoinColumn(name = "store_id")
     private Store store;
 
+    private String description;
+
+    private String image;
+
+    private int inventoryNumber;
+
     public void setId(Long id) {
         this.id = id;
     }
@@ -22,4 +28,50 @@ public class Product {
     }
 
     public Product(){}
+
+    public Product(Store store, String description, String image, int inventoryNumber) {
+        this.store = store;
+        this.description = description;
+        this.image = image;
+        this.inventoryNumber = inventoryNumber;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getInventoryNumber() {
+        return inventoryNumber;
+    }
+
+    public void setInventoryNumber(int inventoryNumber) {
+        this.inventoryNumber = inventoryNumber;
+    }
+
+    public void decreaseInventoryNumber(){
+        this.inventoryNumber--;
+    }
+
+    public void increaseInventoryNumber(){
+        this.inventoryNumber++;
+    }
 }
