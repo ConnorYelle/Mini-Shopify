eb18a2040f7544a250723da0b10dccce2d23a7a5
tommy <tommyphang@cmail.carleton.ca>
2025-11-01 23:13:23 -0400

    Completed Store.java, StoreController, StoreControllerTest, and storeTest for milestone 1

--- src/main/java/com/sysc4806/mini_shopify/StoreController.java
+++ src/main/java/com/sysc4806/mini_shopify/StoreController.java
@@ -1,13 +1,29 @@
 package com.sysc4806.mini_shopify;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Optional;
+
+@RestController
 public class StoreController {
+
+    @Autowired
+    private storeRepository storeRepository;
+
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("title", "Shopify");
         return "index";
+    }
+
+    @PostMapping
+    public Store createStore(@RequestBody Store store) {
+        return storeRepository.save(store);
+    }
+
+    @GetMapping("/store/{id}")
+    public Optional<Store> getStore(@PathVariable Long id) {
+        return storeRepository.findById(id);
     }
 }
