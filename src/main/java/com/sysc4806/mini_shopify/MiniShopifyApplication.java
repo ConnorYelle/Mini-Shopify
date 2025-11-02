ee18e67f8feb1adc075aabe1dbe808b241327db6
ConnorYelle <yelleconnor@gmail.com>
2025-11-02 17:49:45 -0500

    fix: fixed product class so that it has a store property

--- src/main/java/com/sysc4806/mini_shopify/MiniShopifyApplication.java
+++ src/main/java/com/sysc4806/mini_shopify/MiniShopifyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MiniShopifyApplication {
+public class  MiniShopifyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniShopifyApplication.class, args);
--- src/main/java/com/sysc4806/mini_shopify/Product.java
+++ src/main/java/com/sysc4806/mini_shopify/Product.java
@@ -2,11 +2,16 @@ package com.sysc4806.mini_shopify;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Product {
     @Id
     private Long id;
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
 
     public void setId(Long id) {
         this.id = id;
