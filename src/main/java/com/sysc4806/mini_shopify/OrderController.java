be9802647b75c17a13a2bc0771f9a5bce1002055
jonas <jonasandaya@cmail.carleton.ca>
2025-11-03 01:19:33 -0500

    Implemented Order and OrderController. Created an order repository interface called OrderRepository.

--- src/main/java/com/sysc4806/mini_shopify/OrderController.java
+++ src/main/java/com/sysc4806/mini_shopify/OrderController.java
@@ -1,4 +1,26 @@
 package com.sysc4806.mini_shopify;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
 public class OrderController {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("orders", orderRepository.findAll());
+        return "index";
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderRepository.save(order);
+    }
+
+    @GetMapping("/order/{id}")
+    public Optional<Order> getOrder(@PathVariable Long id) {
+        return orderRepository.findById(id);
+    }
 }
